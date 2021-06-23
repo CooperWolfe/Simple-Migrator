5e0cd936a303b22c6127f95835c452f21147d37d
Cooper Wolfe <cooper.wolfe@gmail.com>
2021-06-23 00:41:48 -0400

    made it kinda work

--- src/SimpleMigrator/Program.cs
+++ src/SimpleMigrator/Program.cs
@@ -1,48 +1,49 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using Dapper;
 
 namespace SimpleMigrator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string connectionString, string folder)
         {
             using (var connection = new SqlConnection(connectionString))
-            { 
-                // check if the migrations table exists, otherwise execute the first script (which creates that table) 
-                if (connection.ExecuteScalar<int>(@"SELECT count(1) FROM sys.tables 
-                                                    WHERE T.Name = 'migrationscripts'") == 0) 
-                { 
-                    connection.Execute(GetSql("20151204-1030-Init")); 
-                    connection.Execute(@"INSERT INTO MigrationScripts (Name, ExecutionDate) 
-                                        VALUES (@Name, GETDATE())", 
-                                        new { Name = "20151204-1030-Init" }); 
-                } 
-                                        
-                // Get all scripts that have been executed from the database 
-                var executedScripts = connection.Query<string>("SELECT Name FROM MigrationScripts"); 
+            {
+                // check if the migrations table exists, otherwise execute the first script (which creates that table)
+                if (connection.ExecuteScalar<int>(@"SELECT count(1) FROM sys.tables WHERE T.Name = 'migrationscripts'") == 0)
+                {
+                    connection.Execute(GetSql("00001-Init"));
+                    connection.Execute(
+                        @"INSERT INTO MigrationScripts (Name, ExecutionDate) VALUES (@Name, GETDATE())",
+                        new { Name = "00001-Init" });
+                }
+
+                // Get all scripts that have been executed from the database
+                var executedScripts = connection.Query<string>("SELECT Name FROM MigrationScripts");
                 // Get all scripts from the filesystem
-                Directory.GetFiles(folder) 
-                        // strip out the extensions
-                        .Select(Path.GetFileNameWithoutExtension)
-                        // filter the ones that have already been executed 
-                        .Where(fileName => !executedScripts.Contains(fileName)) 
-                        // Order by the date in the filename 
-                        .OrderBy(fileName => DateTime.ParseExact(fileName.Substring(0, 13), "yyyyMMdd-HHmm", null)) 
-                        .ForEach(script => 
-                        { 
-                            // Execute each one of the scripts 
-                            connection.Execute(GetSql(script)); 
-                            // record that it was executed in the migrationscripts table 
-                            connection.Execute(@"INSERT INTO MigrationScripts (Name, ExecutionDate) 
-                                                VALUES (@Name, GETDATE())", 
-                                                new { Name = script }); 
-                        }); 
-            } 
-        } 
-                
+                var files = Directory.GetFiles(folder)
+                    // strip out the extensions
+                    .Select(fileName => Path.GetFileNameWithoutExtension(fileName)!)
+                    // filter the ones that have already been executed
+                    .Where(fileName => !executedScripts.Contains(fileName))
+                    // order by filename
+                    .OrderBy(fileName => fileName);
+
+                foreach (var file in files)
+                {
+                    // Execute each one of the scripts
+                    connection.Execute(GetSql(file));
+                    // record that it was executed in the migrationscripts table
+                    connection.Execute(
+                        @"INSERT INTO MigrationScripts (Name, ExecutionDate) VALUES (@Name, GETDATE())",
+                        new { Name = file });
+                }
+            }
+        }
+
         static string GetSql(string fileName) => File.ReadAllText(fileName);
     }
 }
